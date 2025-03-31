eea1f1b6b40de727b6ad00deaa436f87761ad9fd
Mitchell Wintrow <90mitchwintrow@gmail.com>
2025-03-31 01:14:43 -0700

    refactor: Create shared TopNav component

--- src/routes/partners/+page.svelte
+++ src/routes/partners/+page.svelte
@@ -1,79 +1,9 @@
 <script>
-  import { page } from '$app/stores';
-  import { Card, Navbar, NavBrand, NavLi, NavUl, NavHamburger, Skeleton } from 'flowbite-svelte';
-  $: activeUrl = $page.url.pathname;
-
-  let hoverStates = {
-    '/': false,
-    '/about': false,
-    '/partners': false,
-    '/careers': false,
-    '/store': false
-  };
-
-  // @ts-ignore
-  function getLinkStyle(href) {
-    const isActive = activeUrl === href;
-    // @ts-ignore
-    const isHovered = hoverStates[href];
-    return `color: ${isHovered ? '#109bd4' : (isActive ? '#109bd4' : '#ffffff')}`;
-  }
+  import { Card } from 'flowbite-svelte';
+  import TopNav from '../../components/TopNav.svelte';
 </script>
 
-<Navbar class="sticky top-0 z-50 shadow-md" style="background-color: #4b7163;">
-  <NavBrand href="/">
-    <img src="/main-logo.png" class="h-10 mr-3" alt="ClearCyle logo" />
-    <span class="self-center text-4xl font-semibold whitespace-nowrap" style="color: #9ed4c3;">ClearCyle</span>
-  </NavBrand>
-  <NavHamburger />
-  <NavUl activeUrl={activeUrl} class="md:flex md:items-center md:space-x-6">
-    <NavLi
-      href="/"
-      class="text-xl"
-      style={getLinkStyle('/')}
-      on:mouseover={() => hoverStates['/'] = true}
-      on:mouseleave={() => hoverStates['/'] = false}
-    >
-      Home
-    </NavLi>
-    <NavLi
-      href="/about"
-      class="text-xl"
-      style={getLinkStyle('/about')}
-      on:mouseover={() => hoverStates['/about'] = true}
-      on:mouseleave={() => hoverStates['/about'] = false}
-    >
-      About Us
-    </NavLi>
-    <NavLi
-      href="/partners"
-      class="text-xl"
-      style={getLinkStyle('/partners')}
-      on:mouseover={() => hoverStates['/partners'] = true}
-      on:mouseleave={() => hoverStates['/partners'] = false}
-    >
-      Partners
-    </NavLi>
-    <NavLi
-      href="/careers"
-      class="text-xl"
-      style={getLinkStyle('/careers')}
-      on:mouseover={() => hoverStates['/careers'] = true}
-      on:mouseleave={() => hoverStates['/careers'] = false}
-    >
-      Careers
-    </NavLi>
-    <NavLi
-      href="/store"
-      class="text-xl"
-      style={getLinkStyle('/store')}
-      on:mouseover={() => hoverStates['/store'] = true}
-      on:mouseleave={() => hoverStates['/store'] = false}
-    >
-      Store
-    </NavLi>
-  </NavUl>
-</Navbar>
+<TopNav />
 
 <main style="background-color: #E8ECEF;">
   <section style="min-height: calc(100vh - 4rem); padding-top: 0rem; background-color: #FFFFFF;" class="flex flex-col items-center">
